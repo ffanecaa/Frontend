07228a6c5c3c8188c736db717824cdb8a08b69de
marta <ffanecaa@gmail.com>
2023-04-17 20:27:57 +0200

    m

--- src/components/Message/Leer.svelte
+++ src/components/Message/Leer.svelte
@@ -1,36 +1,33 @@
+<script>
+ 
+  let name=''
+  let description=''
+  let longuitude=0
+  let latitude=0
+  let elementos = [];
+$: elements = {
+    name: name,
+    description: description,
+    latitude: latitude,
+    longuitude: longuitude,
+  };
 
+  function manexadorTrae(){
+    fetch("http://localhost:8000/elements/")
+      .then(res => res.json())
+      .then(response => {
+        elementos = response;
+      })
+  }
+</script>
 
-<h2> elementos</h2>
+<h2>elementos</h2>
 
-<button on:click = {manexadorTrae}>traer</button> 
+<button on:click={manexadorTrae}>traer</button>
 
 <ul>
-    {#each elementos as {name,description,latitude,longuitude}}
-     <li> {name} {description} {latitude} {longuitude} </li>
-      {/each}
-    </ul>
-
-<script>
-
-
-
-let name
-let description 
-let longuitude
-let latitude
-let elementos=[]
-let element ={
-  name:name,
-  description:description,
-  latitude:latitude,
-  longuitude:longuitude
-}
-
-
-function manexadorTrae () { fetch("http://localhost:8000/users")
-.then(res=>res.json()) 
-    .then(response =>{
-        elementos=response
-    })
-   }
-</script>
+  {#each elementos as {  name, description,latitude, longuitude }}
+    <li> {name} {description} {latitude} {longuitude}</li>
+  {/each}
+</ul>
+<p>{name}</p>
--- src/components/Registro/Registro.svelte
+++ src/components/Registro/Registro.svelte
@@ -1,42 +1,57 @@
 <script>
-    let name
-let description 
-let longuitude
-let latitude
+  let name=''
+  let description=''
+  let longuitude=0
+  let latitude=0
 
-let element ={
-  name:name,
-  description:description,
-  latitude:latitude,
-  longuitude:longuitude
-}
+const elements = {
 
-function manexadorPost(){
-  const datos = fetch ('http://localhost:8000/Element/',{
-    method: 'POST',
-    headers: {'content-type':'application/json'
-  },
-  body:JSON.stringify(element)
-})
-}
+    name: name,
+    description: description,
+    longuitude: longuitude,
+    latitude: latitude,
+  };
 
+  function manexadorPost(){
+  fetch("http://localhost:8000/elements/",{
+      method: "POST",
+      headers:{"Content-Type": "application/json"},
+      body: JSON.stringify(elements),
+  
+  
 
+    })
+  }
 </script>
 
 <div>
-    <h1>datos</h1>
-    <label >nome
-      <input type="text" bind:value={name}>
-    </label><br>
-    <label >description
-      <input type="text" bind:value={description}>
-    </label><br>
-    <label >longuitude
-      <input type="text" bind:value={longuitude}>
-    </label><br>
-    <label >latitude
-      <input type="text" bind:value={latitude}>
-    </label>
-   <button on:click={manexadorPost}>subir</button>
-  </div>
-  
+  <h1>datos</h1>
+ 
+  <label
+    >nome
+    <input type="text" bind:value={name} />
+  </label><br />
+  <label
+    >description
+    <input type="text" bind:value={description} />
+  </label><br />
+  <label
+    >longuitude
+    <input type="number" bind:value={longuitude} />
+  </label><br />
+  <label
+    >latitude
+
+
+
+
+
+
+
+
+
+
+    <input type="number" bind:value={latitude} />
+  </label>
+  <button on:click={manexadorPost}>subir</button>
+</div>
