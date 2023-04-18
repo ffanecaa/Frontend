3d1fab3704593e5e5ac20fafcfbb609a8da6151d
marta <ffanecaa@gmail.com>
2023-04-18 12:28:36 +0200

    m

--- src/components/Elemento/Delete.svelte
+++ src/components/Elemento/Delete.svelte
@@ -3,16 +3,15 @@ export let elemento
 function handleDelete(){
     fetch("http://localhost:8000/elements/",{
 method:"DELETE",
-headers:{"content-Type":"application/json",
+headers:{"content-Type":"application/json"},
 body: JSON.stringify({id: elemento.id})
 }
-    })
+    )
 }
 
 </script>
 
-<h2>borrar</h2>
-<button on:click = {handleDelete}>eliminar</button>
+
+<button on:click = {handleDelete}>❌</button>
 
 
-<style
