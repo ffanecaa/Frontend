c950fb4b053be4877b5e2d70d9f5951a7656c50f
marta <ffanecaa@gmail.com>
2023-04-26 21:19:09 +0200

    fatal

--- src/components/Elemento/Pruebas.svelte
+++ src/components/Elemento/Pruebas.svelte
@@ -1,29 +1,58 @@
-
 <script>
+  let elements = [];
+  let totalPages = 0;
+  let currentPage = 0;
+  const pageSize = 5;
 
- let elementos=[]
-
-
-
-function manexadorTraePrueba(){
-    fetch("http://localhost:8000/pax/")
-      .then(res => res.json())
-      .then(response => {
-        elementos = response;
-      })
+  async function loadElements(page) {
+    const response = await fetch(`http://localhost:8000/elements?page=${page}&size=${pageSize}`);
+    const data = await response.json();
+    elements = data.content;
+    totalPages = data.totalPages;
+    currentPage = page;
   }
 
+  function loadMoreElements() {
+    if (currentPage < totalPages - 1) {
+      loadElements(currentPage + 1);
+    }
+  }
+
+  function loadPreviousPage() {
+    if (currentPage > 0) {
+      loadElements(currentPage - 1);
+    }
+  }
+
+  function loadNextPage() {
+    if (currentPage < totalPages - 1) {
+      loadElements(currentPage + 1);
+    }
+  }
+  
+  loadElements(0);
 </script>
 
-<h2>elementos</h2>
-<button on:click={manexadorTraePrueba}>traer</button>
-<br>
+<main>
+  <button on:click={manexadorelementos}>Cargar elementos</button>
 
+    <ul>
+      {#each elements as element}
+        <li>
+          <h2>{element.name}</h2>
+          <p>{element.description}</p>
+          <p>Latitud: {element.latitude} Longitud: {element.longitude}</p>
+          <img src={element.icon} alt="">
+        </li>
+      {/each}
+    </ul>
+    <div>
+      <button on:click={loadPreviousPage}>Anterior</button>
+      <span>Página {currentPage + 1} de {totalPages}</span>
+      <button on:click={loadNextPage}>Siguiente</button>
+    </div>
 
+    <p>Presiona el botón "Cargar elementos" para cargar los elementos desde el backend</p>
 
-{#each elementos as elemento}
-<h2>{elemento.name}:</h2>
-    <p>{elemento.description}</p>
-    <p>{elemento.latitude} {elemento.longitude}</p>
-    <p>{elemento.icon}</p>
-    {/each}
+</main>
+
