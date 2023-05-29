6f3b38c2e02007f6ca0751e7462354aa340cad13
marta <ffanecaa@gmail.com>
2023-05-29 18:24:57 +0200

    m

--- src/components/Geolocalizacion/Rutas.svelte
+++ src/components/Geolocalizacion/Rutas.svelte
@@ -1,40 +1,33 @@
 <script>
-    import L from "leaflet"
-    import {onMount} from "svelte"
-  import { obterLocalizacion } from "../../lib/geolocation.mjs"
-  import { getElements, getCathegory,getIcon} from "../../lib/fetchs.mjs"
+  import L from "leaflet";
+  import  'leaflet-routing-machine';
+  import { onMount } from "svelte";
+  import { obterLocalizacion } from "../../lib/geolocation.mjs";
+  import { getElements, getCathegory, getIcon } from "../../lib/fetchs.mjs";
+  
 
+  let map;
+  let lat
+let lng
+  
+ 
+ 
 
-
-
-
-
-
-
- let map 
- let coor={}
-let lat 
-let lng 
-
-
-
-  onMount(() => {
-    map = L.map("map").setView([42.812, -7.90005], 8);
+    // Crear el mapa y establecer la vista utilizando la ubicación obtenida
+    map = L.map("map").setView([lat, lng], 8);
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
     }).addTo(map);
-  });
-
-
-    obterLocalizacion()
- console.log(obterLocalizacion);
-    
-  
-
-
-
+    L.Routing.control({
+  waypoints: [
+    L.latLng([lat, lng]),
+    L.latLng([lat,lng ])
+  ]
+}).addTo(map);
 </script>
 
+<!-- <button on:click={ubica}>Obtener Ubicación</button> -->
 
- <div id="map" style="height: 600px;width:800px"></div>
+
+<div id="map" style="height: 600px; width: 800px"></div>
