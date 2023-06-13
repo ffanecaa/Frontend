81d086db00cc2832a5c22fff113019f74a18310b
marta <ffanecaa@gmail.com>
2023-06-13 17:51:05 +0200

    m

--- src/components/Paginacion/PaginacionCategoria.svelte
+++ src/components/Paginacion/PaginacionCategoria.svelte
@@ -117,11 +117,11 @@
        justify-content: space-between;
        align-content: center;
      }
-     .siguientes {
+     /* .siguientes {
        display: flex;
        justify-content: space-between;
        align-content: center;
-     }
+     } */
      .ficha {
        margin-bottom: 80px;
      }
